1d6fb4ff93545fbaf31a3f3ecf8435388c550c65
Anesh Parvatha <anesh@cybereyelabs.io>
2020-01-24 12:30:40 +0530

    Integrate table data API call

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
-import { tableShape } from './types';
+import { pokerGameService } from './types';
 
 import Table from './components/Table';
 
 import './App.css';
 
+function Loader() {
+  return (
+    <div className="Table">
+      <h1 className="loader">Setting up the table...</h1>
+    </div>
+  );
+}
+
 class App extends Component {
   static propTypes = {
-    table: tableShape.isRequired,
+    pokerGameService: pokerGameService.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      table: null,
+      loading: false
+    };
   }
 
+  componentDidMount() {
+    const { pokerGameService } = this.props;
+    this.setState({
+      loading: true
+    });
+    const request = {
+      tableId: 1
+    };
+    const tableDataPromise = pokerGameService.getTableData(request);
+    tableDataPromise.then(table => {
+      this.setState({
+        table: table,
+        loading: false
+      });
+    });
+  }
+
+  isLoading = () => {
+    const { table, loading } = this.state;
+    return loading || table === null;
+  };
+
   render() {
-    const { table } = this.props;
+    const { table } = this.state;
 
     return (
       <div className="App">
-        <Table table={table} />
+        {this.isLoading() ? <Loader /> : <Table table={table} />}
       </div>
     );
   }
--- src/types.js
+++ src/types.js
@@ -6,27 +6,27 @@ const cardType = PropTypes.string;
 const playerShape = PropTypes.shape({
   seatId: PropTypes.number.isRequired,
   bet: PropTypes.number.isRequired,
-  cards: PropTypes.arrayOf(cardType).isRequired,
+  cards: PropTypes.arrayOf(cardType).isRequired
 });
 
 export const seatProps = {
   id: PropTypes.number.isRequired,
   state: PropTypes.oneOf(['available', 'occupied']).isRequired,
   username: PropTypes.string,
-  chips: PropTypes.number,
+  chips: PropTypes.number
 };
 export const seatShape = PropTypes.shape(seatProps);
 
 const potShape = PropTypes.shape({
   chips: PropTypes.number.isRequired,
-  seatIds: PropTypes.arrayOf(PropTypes.number).isRequired,
+  seatIds: PropTypes.arrayOf(PropTypes.number).isRequired
 });
 
 const handShape = PropTypes.shape({
   id: PropTypes.number.isRequired,
   communityCards: PropTypes.arrayOf(cardType).isRequired,
   players: PropTypes.arrayOf(playerShape).isRequired,
-  pots: PropTypes.arrayOf(potShape).isRequired,
+  pots: PropTypes.arrayOf(potShape).isRequired
 });
 
 export const tableShape = PropTypes.shape({
@@ -35,8 +35,12 @@ export const tableShape = PropTypes.shape({
   game: PropTypes.oneOf(['holdem', 'omaha', 'royal']).isRequired,
   blinds: PropTypes.shape({
     small: PropTypes.number.isRequired,
-    big: PropTypes.number.isRequired,
+    big: PropTypes.number.isRequired
   }).isRequired,
   seats: PropTypes.arrayOf(seatShape).isRequired,
   currentHand: handShape
+});
+
+export const pokerGameService = PropTypes.shape({
+  getTableData: PropTypes.func.isRequired
 });
