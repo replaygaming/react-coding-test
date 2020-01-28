4da65499a85d8ce825c19472ea518f5481220118
Ionut Cirja <ionut.cirja@gmail.com>
2020-01-28 11:58:00 +0000

    create tables list mock data

---
+++ src/data/tables.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    name: 'Table 1',
+  },
+  {
+    id: 2,
+    name: 'Table 2',
+  },
+  {
+    id: 3,
+    name: 'Table 3',
+  },
+  {
+    id: 4,
+    name: 'Table 4',
+  },
+  {
+    id: 5,
+    name: 'Table 5',
+  },
+  {
+    id: 6,
+    name: 'Table 6',
+  },
+  {
+    id: 7,
+    name: 'Table 7',
+  },
+  {
+    id: 8,
+    name: 'Table 8',
+  },
+  {
+    id: 9,
+    name: 'Table 9',
+  },
+];
